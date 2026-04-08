7e683ddeea6219f68981acc74de23e9a4622c1aa
pedrodalagnol <joao.rossatti@edu.pucs.br>
2026-04-08 11:39:24 -0300

    Add data model and converter for series information
    - Introduced DadosSerie record to represent series data.
    - Implemented ConverterDados class for JSON to object conversion.
    - Updated ScreenmatchApplication to utilize new classes.
    - Added Jackson databind dependency in pom.xml.

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverterDados;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner{
@@ -17,7 +19,13 @@ public class ScreenmatchApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		var ConsumoAPI = new ConsumoAPI();
 		var json = ConsumoAPI.obterDados("http://www.omdbapi.com/?apikey=70c8bcd5&t=Gilmore+Girls");
+		
 		System.out.println(json);
+
+		ConverterDados conversor = new ConverterDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+
+		System.out.println(dados);
 	}
 
 }
